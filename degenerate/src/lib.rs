1d37aa064b3c5a57993212cc6eba6bca28f5ed3a
Ulrich Berthold <u@sansculotte.net>
2020-02-12 10:21:22 +0100

    attempts at algorithmic pixel generation

---
+++ degenerate/src/lib.rs
@@ -0,0 +1,9 @@
+/*
+pub fn new_x_001(cs: f64, x: u32, y: u32, z:f64, c:f64) -> u32 {
+    (cx + c.sin() * r + (c * 1.777).cos() * z * (imgy as f64)).round() as u32
+}
+
+pub fn new_y_001(cs: f64, x: u32, y: u32, z:f64, c:f64) -> u32 {
+    (cy + c.cos() * r + (c * 1.666).sin() * ((x as f64 * z * (imgx as f64))).sqrt() as f64).round() as u32
+}
+*/
---
+++ degenerate/src/main.rs
@@ -0,0 +1,66 @@
+extern crate image;
+extern crate rand;
+
+use std::f64::consts::{E, PI, SQRT_2};
+use rand::random;
+
+const PHI: f64 = 1.618033988749;
+
+fn main() {
+
+    let imgx = 4000;
+    let imgy = 4000;
+
+    // Create a new ImgBuf with width: imgx and height: imgy
+    let mut imgbuf = image::ImageBuffer::new(imgx, imgy);
+
+    // Iterate over the coordinates and pixels of the image
+    for (_, _, pixel) in imgbuf.enumerate_pixels_mut() {
+        *pixel = image::Luma([0]);
+    }
+
+    let cx: f64 = imgx as f64 / 2.;
+    let cy: f64 = imgy as f64 / 2.;
+    let mut r: f64 = 235.0;
+    let iterations: u32 = imgx*imgy * 64; // arbitrary
+
+    let mut c: f64;
+    let mut c2: f64;
+    let mut c3: f64;
+    let mut x: u32;
+    let mut y: u32;
+    let mut z: f64 = 0.;
+    let mut zs = vec![vec![0f64; imgy as usize]; imgx as usize];
+
+    for i in 0..iterations {
+
+        c = (i as f64 / iterations as f64) * PI * 2.0;
+        c2 = c * E;
+        c3 = c * PHI;
+        x = (cx + c.sin() * r + c.cos().powf(2.0) * z * (imgy as f64)).round() as u32 % imgx;
+        y = (cy + c.cos() * r + (c * PHI) * ((x as f64 * z * (imgx as f64))).sqrt() as f64).round() as u32 % imgy;
+        z = (
+                //(x as f64).sin() * (i as f64) + (y as f64).cos() * 2.3f64.powf(x as f64)
+                /*
+                ((((x as f64).sin() * PI * (y as f64 + z)).cos() + c.sqrt() + (E * c.cos()) * (SQRT_2 * (y as f64).cos()) * c3.sqrt() * c2.cos()).atan() / 1.569796)
+                *
+                ((((x as f64).sin() * PI * (y as f64 + z)).cos() + (c + z).powf(E) + (E * c.cos()) * (SQRT_2 * (y as f64).powf(3.).cos()) * (c3 * c2).sqrt() * c2.cos().powf(2.)).atan() / 1.569796)
+                */
+                //c.cos() * c.tan() * c3.cos() * (x as f64 + z.powf(c)).sin()
+                (c.powf(SQRT_2).cos() * c2.powf(3.).tan() * c3.powf(2.).cos() * (x as f64 + z.powf(c)).sin()).atan() / 1.569796
+        ).abs();
+        // atan saturates at ~ 1.569796
+        zs[x as usize][y as usize] += z;
+
+//        let pixel = imgbuf.get_pixel_mut(x, y);
+//        let data = (*pixel as image::Luma<u8>).0;
+//        *pixel = image::Luma([((data[0] as u32 + z as u32) % 255) as u8]);
+        //*pixel = image::Luma([z as u8]);
+    }
+
+    for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
+        *pixel = image::Luma([(zs[x as usize][y as usize] * 255.) as u8]);
+    }
+
+    imgbuf.save("image.png").unwrap();
+}
