363fc1abf9fd3e1d87a42658066c8c75ab8e7a87
Ulrich Berthold <u@sansculotte.net>
2020-02-22 15:10:57 +0100

    gotta hold that moment

--- degenerate/src/main.rs
+++ degenerate/src/main.rs
@@ -24,17 +24,18 @@ fn main() {
         *pixel = image::Luma([0]);
     }
 
+    let iterations: u32 = imgx*imgy * 64; // arbitrary
+
     let cx: f64 = imgx as f64 / 2.;
     let cy: f64 = imgy as f64 / 2.;
     let mut r: f64 = 235.0;
-    let iterations: u32 = 20000; // imgx*imgy; // arbitrary
 
     let mut c: f64;
     let mut c2: f64;
     let mut c3: f64;
     let mut x: u32;
     let mut y: u32;
-    let mut z: f64 = 0.;
+    let mut z: f64 = 1.;
     let mut zs = vec![vec![0f64; imgy as usize]; imgx as usize];
 
     for i in 0..iterations {
@@ -42,18 +43,19 @@ fn main() {
         c = (i as f64 / iterations as f64) * PI * 2.0;
         c2 = c * E;
         c3 = c * PHI;
-        x = (cx + c.sin() * r + c.cos().powf(2.0) * z * (imgy as f64)).round() as u32 % imgx;
-        y = (cy + c.cos() * r + (c * PHI) * ((x as f64 * z * (imgx as f64))).sqrt() as f64).round() as u32 % imgy;
+        x = (cx + c.sin() * r + c.cos() * z * (imgy as f64)).round() as u32 % imgx;
+        y = (cy + c.cos() * r + (c * z).sin() * (x as f64 * z * (imgx as f64)).sqrt() as f64).round() as u32 % imgy;
         z = (
                 //(x as f64).sin() * (i as f64) + (y as f64).cos() * 2.3f64.powf(x as f64)
                 ((((x as f64).sin() * PI * (y as f64 + z)).cos() + c.sqrt() + (E * c.cos()) * (SQRT_2 * (y as f64).cos()) * c3.sqrt() * c2.cos()).atan() / ATAN_SATURATION)
                 *
                 ((((x as f64).sin() * PI * (y as f64 + z)).cos() + (c + z).powf(E) + (E * c.cos()) * (SQRT_2 * (y as f64).powf(3.).cos()) * (c3 * c2).sqrt() * c2.cos().powf(2.)).atan() / ATAN_SATURATION)
                 //c.cos() * c.tan() * c3.cos() * (x as f64 + z.powf(c)).sin()
-                // (c.powf(SQRT_2).cos() * c2.powf(3.).tan() * c3.powf(2.).cos() * (x as f64 + z.powf(c)).sin()).atan() / ATAN_SATURATION
+                //(c.powf(SQRT_2).cos() * c2.powf(3.).tan() * c3.powf(2.).cos() * (x as f64 + z.powf(c)).sin()).atan() / ATAN_SATURATION
         ).abs();
-        // zs[x as usize][y as usize] += z;
-        draw_shape(&mut zs, x, y, z);
+        r -= 235.0 / iterations as f64;
+        zs[x as usize][y as usize] += z;
+        // draw_shape(&mut zs, x, y, z);
 
 //        let pixel = imgbuf.get_pixel_mut(x, y);
 //        let data = (*pixel as image::Luma<u8>).0;
