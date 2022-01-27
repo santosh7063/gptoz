57c8e8e2e2c5d10829a50b15689e7e478a5a4356
Ulrich Berthold <u@sansculotte.net>
2022-01-27 10:53:10 +0100

    add parameters: negative, mix, start and length

--- convolve.py
+++ convolve.py
@@ -6,8 +6,10 @@ import numpy as np
 from os import path
 from glob import glob
 from itertools import cycle
+from aubio import fvec, level_lin
 from audiopack import loadwav, audio_chunks, spectrum
 from scipy.signal import fftconvolve
+from scipy.interpolate import interp1d
 
 
 if __name__ == '__main__':
@@ -29,7 +31,13 @@ if __name__ == '__main__':
     parser.add_argument('-m', '--mode', dest='mode', type=str, action='store', default='signal',
         help='signal: time domain, fft: frequency domain'
     )
-    parser.add_argument('-a', '--amplify', dest='amplify', type=float, action='store', default=100,
+    parser.add_argument('-n', '--negative', dest='negative', action='store_true',
+        help='negative'
+    )
+    parser.add_argument('-M', '--mix', dest='mix', action='store_true',
+        help='mix by loudness'
+    )
+    parser.add_argument('-a', '--amplify', dest='amplify', type=float, action='store',
         help='amplify signal or spectrum'
     )
     parser.add_argument('-W', '--width', dest='width', type=int, action='store', default=1280,
@@ -44,12 +52,21 @@ if __name__ == '__main__':
     parser.add_argument('-t', '--trigger', dest='trigger', action='store_true',
         help='Stabilize on waveform features'
     )
+    parser.add_argument('--start', dest='start', type=int, action='store', default=0,
+        help='Start frame'
+    )
+    parser.add_argument('--length', dest='length', type=int, action='store', default=0,
+        help="Length in video frames"
+    )
     args = parser.parse_args()
 
     meta, data = loadwav(args.soundfile)
 
     blocksize: int = meta.rate // args.fps
     blocks: int    = meta.samples // blocksize
+
+    start: int = args.start
+    length = args.length if args.length > 0 else blocks-start
 
     files = glob(args.infile)
     if args.shape:
@@ -61,16 +78,40 @@ if __name__ == '__main__':
     else:
         shape = None
 
+    if args.amplify is None:
+        if args.mode == 'fft':
+            amplify = 100
+        elif args.mode == 'signal':
+            amplify = 1
+    else:
+        amplify = args.amplify
+
     for n, data_pair in enumerate(zip(audio_chunks(data, blocksize), cycle(files))):
+        if n < start:
+            continue
+        if n > start+length:
+            break
+
         block, imgfile = data_pair
         padded = "{0:05d}".format(n)
         bitmap = cv2.imread(imgfile, cv2.IMREAD_GRAYSCALE)
-        if args.mode == 'fft':
+        level = level_lin(fvec(block.T[0]))
+
+        if args.mode == 'fft' and args.mix:
+            block = spectrum(block, meta.rate, bins=(1 + int(level * blocksize)))
+        elif args.mode == 'fft':
             block = spectrum(block, meta.rate)
+        elif args.mode == 'signal' and args.mix:
+#            block = np.resize(block, (1 + int(level * blocksize), ))
+            block = block[0:1 + int(level * blocksize)]
+
         if shape:
             block = np.reshape(block, shape)
+        if args.negative:
+            block = np.max(block) / 2 - block
 
-        image = fftconvolve(bitmap, block * args.amplify, mode='same')
+        image = fftconvolve(bitmap, block * amplify, mode='same')
+
         cv2.imwrite(path.join(args.outdir, f'{padded}.png'), image)
 
         percent_finished = int(n / blocks * 100)
